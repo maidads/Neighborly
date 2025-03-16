91c49339bd26ef0b17d9c109c1eeb8f17f29abf7
Maida <98819095+maidads@users.noreply.github.com>
2025-03-16 14:54:42 +0100

    added navbar and icons

--- app/src/main/java/com/example/myneighborly/MainActivity.kt
+++ app/src/main/java/com/example/myneighborly/MainActivity.kt
@@ -1,7 +1,37 @@
 package com.example.myneighborly
 
-import androidx.activity.ComponentActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNav)
+
+        // Lägga till lyssnare för BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment = HomeFragment()  // Standardfragment
+
+            when (item.itemId) {
+                R.id.navigation_help -> selectedFragment = HomeFragment()
+                R.id.navigation_chats -> selectedFragment = ChatsFragment()
+                R.id.navigation_profile -> selectedFragment = ProfileFragment()
+            }
+
+            // Ersätt fragmentet i FrameLayout
+            //supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+
+            true
+        }
+
+        // Sätt standardfragment
+        if (savedInstanceState == null) {
+            //supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+        }
+    }
 }
