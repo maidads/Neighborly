e2ba2d7d9ec46bf7c7d156d0de1594b75bbc7dbd
Maida <98819095+maidads@users.noreply.github.com>
2025-03-18 10:46:44 +0100

    fixing top bar

--- app/src/main/java/com/example/myneighborly/MainActivity.kt
+++ app/src/main/java/com/example/myneighborly/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.myneighborly
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
+    private lateinit var toolbarIcon: ImageView
+    private lateinit var toolbarTitle: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbarIcon = findViewById(R.id.toolbarIcon)
+        toolbarTitle = findViewById(R.id.toolbarTitle)
+
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         /*
         val logOutButton: Button = findViewById(R.id.logOutButton)
@@ -27,20 +41,44 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             var selectedFragment: Fragment = HomeFragment()
+            var selectedIcon: Int = R.drawable.home_icon
+            var selectedTitle: String = "Help"
 
             when (item.itemId) {
-                R.id.navigation_help -> selectedFragment = HomeFragment()
-                R.id.navigation_chats -> selectedFragment = ChatsFragment()
-                R.id.navigation_profile -> selectedFragment = ProfileFragment()
+                R.id.navigation_search -> {
+                    selectedFragment = SearchFragment()
+                    selectedIcon = R.drawable.search_icon
+                    selectedTitle = "Search"
+                }
+                R.id.navigation_help -> {
+                    selectedFragment = HomeFragment()
+                    selectedIcon = R.drawable.home_icon
+                    selectedTitle = "Help"
+                }
+                R.id.navigation_chats -> {
+                    selectedFragment = ChatsFragment()
+                    selectedIcon = R.drawable.chat_icon
+                    selectedTitle = "Chats"
+                }
+                R.id.navigation_profile -> {
+                    selectedFragment = ProfileFragment()
+                    selectedIcon = R.drawable.person_icon
+                    selectedTitle = "My profile"
+                }
             }
 
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+
+            toolbarIcon.setImageResource(selectedIcon)
+            toolbarTitle.text = selectedTitle
 
             true
         }
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+            toolbarIcon.setImageResource(R.drawable.home_icon)
+            toolbarTitle.text = "Home"
         }
     }
 }
