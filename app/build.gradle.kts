896760cf53254256870adcc27d6f2da128d73329
Maida <98819095+maidads@users.noreply.github.com>
2025-03-19 21:32:15 +0100

    adding horizontel types of help with images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.0")
     implementation( platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.0")
 
     implementation ("com.google.firebase:firebase-auth")
     implementation ("com.google.firebase:firebase-firestore")
